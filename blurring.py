2d354ac0d5157cea262f770d92e377c2ed2abdda
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-07-09 16:00:53 +0530

    gaussian blurring and median blurring process added

--- blurring.py
+++ blurring.py
@@ -8,12 +8,25 @@ image = cv2.imread(os.path.join('C:\\Users', 'Soumyajit Koley', 'Downloads', 'OI
 
 #### method 1 --> CLASSICAL BLURRING ...................
 
-blur_img = cv2.blur(image, (5,5))
+ksize = 5
+blur_img = cv2.blur(image, (ksize,ksize))
 blur_img1 = cv2.blur(image, (10,5))
 
 cv2.imshow('frame',image)
 cv2.imshow('frame1',blur_img)
 cv2.imshow('frame2',blur_img1)
+
+#### method 2 ---> GAUSSIAN BLURRING ......................
+
+gaus_blur_img = cv2.GaussianBlur(image,(ksize,ksize),10,5)
+
+cv2.imshow('frame3',gaus_blur_img)
+
+#### method 3 ---> MEDIAN BLURRING .....................
+
+mid_img = cv2.medianBlur(image,ksize)
+
+cv2.imshow('frame4',mid_img)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
