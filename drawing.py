11c028cab25fb6c86acd250f00df9698e56ea54e
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-07-17 10:45:07 +0530

    shape like rectangle circle drawing using opencv

--- drawing.py
+++ drawing.py
@@ -2,14 +2,26 @@ import cv2
 
 import os
 
+import cv2.text
+
 image_path = os.path.join('C:\\Users', 'Soumyajit Koley', 'Downloads', 'whiteboard.jpeg')
 image = cv2.imread(image_path)
 
 #   how to draw line ......................................
 
-cv2.line(image,(100,80),(200,80),(255,0,0),1)
+cv2.line(image,(50,30),(100,30),(255,0,0),1)
 
 # in line (1st image --> 1st point coordinate --> 2nd point coordinate --> color --> thickness)
+
+#   how to draw rectangle ..................................
+
+cv2.rectangle(image,(50,50),(100,100),(0,255,0),3)  # upper co , lower co , color , thickness
+
+#   how to draw circle ...................................
+
+cv2.circle(image,(150,70),30,(0,0,255),3)   # center , radius , color , thickness
+
+
 
 
 cv2.imshow('frame',image)
