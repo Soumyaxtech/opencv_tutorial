c987232a73299ae722a41ac717c0dcf3fc14c5a8
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-07-18 23:26:41 +0530

    drawing contours and bounding them with rectangle using boundingRect function added

--- contours.py
+++ contours.py
@@ -13,6 +13,15 @@ contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIM
 for cnt in contours:
     print(cv2.contourArea(cnt))
 
+for cnt in contours:
+    if cv2.contourArea(cnt)>1200:
+        cv2.drawContours(image,cnt,-1,(0,0,255),1)
+
+        x1,y1,w,h = cv2.boundingRect(cnt)
+        
+        cv2.rectangle(image,(x1,y1),(x1+w,y1+h),(255,0,0),2)
+    
+    
 
 cv2.imshow('frame',image)
 cv2.imshow('frame1',img_gray)
